5055bf1bfbbb321c062d2b45082b083744966d7d
Ross Higgins <rossinihiggins@gmail.com>
2019-12-16 01:44:18 -0800

    cleaning imports

--- src/tests/ApiTests.java
+++ src/tests/ApiTests.java
@@ -4,17 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.logging.Log;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
